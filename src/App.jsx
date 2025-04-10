56071bc08c83a03c5d74a61a985a0c9bbc4d8257
Alex P <afapenha@gmail.com>
2025-04-10 17:49:07 -0400

    made it so pages can be rendered independently from the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InterfaceOutput from "./Global/Bars/InterfaceOuput/InterfaceOuput";
+import IsLoggedInCheck from "./auth/IsLoggedInCheck";
+
 import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/Login/LoginPage";
-import IsLoggedInCheck from "./auth/IsLoggedInCheck";
+import RegisterPage from "./pages/Register/RegisterPage";
+import AboutPage from "./pages/About/AboutPage";
+import PageNotFound from "./pages/Error/PageNotFound";
 
 const App = () => {
   return (
@@ -12,8 +16,13 @@ const App = () => {
           <Route path="/" element={<InterfaceOutput />}>
             <Route index element={<HomePage />} />
             <Route path="home" element={<HomePage />} />
-            <Route path="login" element={<LoginPage />} />
+            <Route path="about" element={<AboutPage />} />
           </Route>
+
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
     </IsLoggedInCheck>
