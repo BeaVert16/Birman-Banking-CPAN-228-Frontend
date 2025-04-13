7b482f331c44234423c8b52bdf2ebceb2344e298
Alex P <afapenha@gmail.com>
2025-04-12 23:30:09 -0400

    footer stuff that probably didnt have to change

--- src/Global/Bars/Footer/Footer.jsx
+++ src/Global/Bars/Footer/Footer.jsx
@@ -2,9 +2,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <p>© 2025 Birman Banking. All rights reserved.</p>
-    </div>
+    <div className="footer">© 2025 Birman Banking. All rights reserved.</div>
   );
 };
 
